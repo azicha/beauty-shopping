a666ab3398dd85e20f58529b9290b52ba12c2ac3
azicha <askakauchi@gmail.com>
2023-04-08 10:53:39 +0300

    Configure Tailwind and DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.{html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+
+  daisyui: {
+    prefix: "daisy-",
+  }
+}
+
